35719aa7a1dc6edbe79ff455c4aeec974f50c6cc
Jade Wiegel <jadewiegel@gmail.com>
2023-02-27 19:51:32 -0600

    userPage inputs for new veh are on the page. nothing is saving inputs yet at this time.

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -9,6 +9,24 @@ function UserPage() {
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+
+      <h2>Select Your Vehicle</h2>
+
+      {/* if there are any vehicles that user has input display here */}
+
+      <br />
+      <form className="newVehicleInput">
+      {/* input for vehicle year */}
+      <input placeholder="Vehicle Year"></input><br />
+      {/* input for vehicle make */}
+      <input placeholder="Vehicle Make"></input><br />
+      {/* input for vehicle model */}
+      <input placeholder="Vehicle Model"></input><br />
+      {/* button to submit vehicle */}
+      <button>Submit</button>
+      </form>
+      <br />
+      <br />
       <LogOutButton className="btn" />
     </div>
   );
